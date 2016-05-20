243d2ceaaaf01d77bb50cd21982afa763730b313
mthakkar <mthakkar@cloudera.com>
2016-05-20 11:51:01 -0700

    [KAFKA] Updated source

--- KAFKA/src/scripts/control.sh
+++ KAFKA/src/scripts/control.sh
@@ -22,20 +22,32 @@ set -x
 
 DEFAULT_KAFKA_HOME=/usr/lib/kafka
 KAFKA_HOME=${KAFKA_HOME:-$DEFAULT_KAFKA_HOME}
+MIN_KAFKA_MAJOR_VERSION_WITH_SSL=2
 
 # For better debugging
 echo ""
 echo "Date: `date`"
-echo "Host: `hostname -f`"
+echo "Host: $HOST"
 echo "Pwd: `pwd`"
 echo "CONF_DIR: $CONF_DIR"
 echo "KAFKA_HOME: $KAFKA_HOME"
 echo "Zoookeper Quorum: $ZK_QUORUM"
 echo "Chroot: $CHROOT"
+echo "PORT: $PORT"
 echo "JMX_PORT: $JMX_PORT"
+echo "SSL_PORT: $SSL_PORT"
 echo "ENABLE_MONITORING: ${ENABLE_MONITORING}"
 echo "METRIC_REPORTERS: ${METRIC_REPORTERS}"
 echo "BROKER_HEAP_SIZE: ${BROKER_HEAP_SIZE}"
+echo "KERBEROS_AUTH_ENABLED: ${KERBEROS_AUTH_ENABLED}"
+echo "KAFKA_PRINCIPAL: ${KAFKA_PRINCIPAL}"
+echo "SECURITY_INTER_BROKER_PROTOCOL: ${SECURITY_INTER_BROKER_PROTOCOL}"
+echo "AUTHENTICATE_ZOOKEEPER_CONNECTION: ${AUTHENTICATE_ZOOKEEPER_CONNECTION}"
+
+if [[ -z ${ZK_PRINCIPAL_NAME} ]]; then
+    ZK_PRINCIPAL_NAME="zookeeper"
+fi
+echo "ZK_PRINCIPAL_NAME: ${ZK_PRINCIPAL_NAME}"
 
 # Generating Zookeeper quorum
 QUORUM=$ZK_QUORUM
@@ -53,6 +65,107 @@ if [[ ${ENABLE_MONITORING} == "true" ]]; then
     echo "kafka.metrics.reporters=${METRIC_REPORTERS}" >>  $CONF_DIR/kafka.properties
 fi
 
+# Add SSL parameters
+if [[ ${BROKER_SSL_ENABLED} == "true" ]]; then
+    # Make sure kafka version is greater than or equal to 2.0.0
+    if [[ ! -f $KAFKA_HOME/cloudera/cdh_version.properties ]]; then
+        echo "$KAFKA_HOME/cloudera/cdh_version.properties not found. Assuming older version of Kafka is being used that does not support SSL."
+        exit 1
+    else
+        KAFKA_VERSION=$(grep "^version=" $KAFKA_HOME/cloudera/cdh_version.properties | cut -d '=' -f 2)
+        KAFKA_MAJOR_VERSION=$(echo $KAFKA_VERSION | cut -d '-' -f 2 | sed 's/kafka//g' | cut -d '.' -f 1)
+        if [[ $KAFKA_MAJOR_VERSION < $MIN_KAFKA_MAJOR_VERSION_WITH_SSL ]]; then
+            echo "Kafka version, $KAFKA_VERSION, does not support SSL"
+            exit 1
+        fi
+    fi
+
+    set +x
+    # Append other ssl params from ssl.properties
+    SSL_CONFIGS=$(cat ssl.properties)
+
+    # If user has not provided safety valve, replace SSL_CONFIGS's placeholder
+    perl -pi -e "s#\#ssl.configs={{SSL_CONFIGS}}#${SSL_CONFIGS}#" $CONF_DIR/kafka.properties
+    set -x
+fi
+
+# Generate JAAS config file
+if [[ ${KERBEROS_AUTH_ENABLED} == "true" ]]; then
+    # If user has not provided safety valve, replace JAAS_CONFIGS's placeholder
+    if [ -z "$JAAS_CONFIGS" ]; then
+        KEYTAB_FILE="${CONF_DIR}/kafka.keytab"
+        JAAS_CONFIGS="
+KafkaServer {
+   com.sun.security.auth.module.Krb5LoginModule required
+   doNotPrompt=true
+   useKeyTab=true
+   storeKey=true
+   keyTab=\"$KEYTAB_FILE\"
+   principal=\"$KAFKA_PRINCIPAL\";
+};
+"
+        if [[ ${AUTHENTICATE_ZOOKEEPER_CONNECTION} == "true" ]]; then
+            JAAS_CONFIGS="${JAAS_CONFIGS}
+
+Client {
+   com.sun.security.auth.module.Krb5LoginModule required
+   useKeyTab=true
+   storeKey=true
+   keyTab=\"$KEYTAB_FILE\"
+   principal=\"$KAFKA_PRINCIPAL\";
+};"
+        fi
+    fi
+    echo "${JAAS_CONFIGS}" > $CONF_DIR/jaas.conf
+fi
+
+
+# Security protocol to be used
+SECURITY_PROTOCOL=""
+if [[ ${KERBEROS_AUTH_ENABLED} == "true" ]]; then
+    if [[ ${BROKER_SSL_ENABLED} == "true" ]]; then
+        SECURITY_PROTOCOL="SASL_SSL"
+    else
+        SECURITY_PROTOCOL="SASL_PLAINTEXT"
+    fi
+else
+    if [[ ${BROKER_SSL_ENABLED} == "true" ]]; then
+        SECURITY_PROTOCOL="SSL"
+    else
+        SECURITY_PROTOCOL="PLAINTEXT"
+    fi
+fi
+
+
+# Add listeners
+LISTENERS="listeners="
+if [[ ${BROKER_SSL_ENABLED} == "true" ]]; then
+    LISTENERS="${LISTENERS}${SECURITY_PROTOCOL}://${HOST}:${SSL_PORT},"
+else
+    LISTENERS="${LISTENERS}${SECURITY_PROTOCOL}://${HOST}:${PORT},"
+fi
+if [[ ${SECURITY_INTER_BROKER_PROTOCOL} != ${SECURITY_PROTOCOL} ]]; then
+    # Verify SSL or SASL can be set, if included in security.inter.broker.protocol
+    if [[ ${SECURITY_INTER_BROKER_PROTOCOL} == *"SSL"* && ${BROKER_SSL_ENABLED} != "true" ]]; then
+        echo "security.inter.broker.protocol can not be set to ${SECURITY_INTER_BROKER_PROTOCOL}, as SSL is not enabled on this Kafka broker."
+        exit 1
+    fi
+    if [[ ${SECURITY_INTER_BROKER_PROTOCOL} == *"SASL"* && ${KERBEROS_AUTH_ENABLED} != "true" ]]; then
+        echo "security.inter.broker.protocol can not be set to ${SECURITY_INTER_BROKER_PROTOCOL}, as Kerberos is not enabled on this Kafka broker."
+        exit 1
+    fi
+
+
+    if [[ ${SECURITY_INTER_BROKER_PROTOCOL} == *"SSL"* ]]; then
+        LISTENERS="${LISTENERS}${SECURITY_INTER_BROKER_PROTOCOL}://${HOST}:${SSL_PORT},"
+    else
+        LISTENERS="${LISTENERS}${SECURITY_INTER_BROKER_PROTOCOL}://${HOST}:${PORT},"
+    fi
+fi
+echo "LISTENERS=${LISTENERS}"
+
+# Replace LISTENERS's placeholder
+perl -pi -e "s#\#listeners={{LISTENERS}}#${LISTENERS}#" $CONF_DIR/kafka.properties
 
 # Propoagating logger information to Kafka
 export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:$CONF_DIR/log4j.properties"
@@ -65,6 +178,15 @@ export KAFKA_HEAP_OPTS="-Xmx${BROKER_HEAP_SIZE}M"
 
 # Set java opts
 export KAFKA_JVM_PERFORMANCE_OPTS="${BROKER_JAVA_OPTS}"
+
+# Set KAFKA_OPTS for security
+if [[ ${KERBEROS_AUTH_ENABLED} == "true" ]]; then
+    export KAFKA_OPTS="${KAFKA_OPTS} -Djava.security.auth.login.config=${CONF_DIR}/jaas.conf"
+
+    if [[ ${AUTHENTICATE_ZOOKEEPER_CONNECTION} == "true" ]]; then
+      export KAFKA_OPTS="${KAFKA_OPTS} -Dzookeeper.sasl.client.username=${ZK_PRINCIPAL_NAME}"
+    fi
+fi
 
 # And finally run Kafka itself
 exec $KAFKA_HOME/bin/kafka-server-start.sh $CONF_DIR/kafka.properties
--- KAFKA/src/scripts/mirrormaker_control.sh
+++ KAFKA/src/scripts/mirrormaker_control.sh
@@ -36,48 +36,134 @@ echo "num.producers: ${NUM_PRODUCERS}"
 echo "num.streams: ${NUM_STREAMS}"
 echo "queue.size: ${QUEUE_SIZE}"
 echo "JMX_PORT: $JMX_PORT"
+echo "abort.on.send.failure: ${ABORT_ON_SEND_FAILURE}"
+echo "offset.commit.interval.ms: ${OFFSET_COMMIT_INTERVAL_MS}"
+echo "consumer.rebalance.listener: ${CONSUMER_REBALANCE_LISTENER}"
+echo "consumer.rebalance.listener.args: ${CONSUMER_REBALANCE_LISTENER_ARGS}"
+echo "message.handler: ${MESSAGE_HANDLER}"
+echo "message.handler.args: ${MESSAGE_HANDLER_ARGS}"
+echo "SOURCE_SECURITY_PROTOCOL: ${SOURCE_SECURITY_PROTOCOL}"
+echo "DESTINATION_SECURITY_PROTOCOL: ${DESTINATION_SECURITY_PROTOCOL}"
+echo "KAFKA_MIRROR_MAKER_PRINCIPAL: ${KAFKA_MIRROR_MAKER_PRINCIPAL}"
+echo "SOURCE_SSL_CLIENT_AUTH: ${SOURCE_SSL_CLIENT_AUTH}"
+echo "DESTINATION_SSL_CLIENT_AUTH: ${DESTINATION_SSL_CLIENT_AUTH}"
 
-# Generating Zookeeper quorum
-QUORUM=$ZK_QUORUM
-if [[ -n $CHROOT ]]; then
-    QUORUM="${QUORUM}${CHROOT}"
-fi
-echo "Final Zookeeper Quorum is $QUORUM"
-
-if ! grep zookeeper.connect= ${CONF_DIR}/mirror_maker_consumers.properties; then
-    echo "zookeeper.connect=$QUORUM" >> ${CONF_DIR}/mirror_maker_consumers.properties
-fi
-
-if [[ ${NO_DATA_LOSS} == "true" ]]; then
-    DATA_LOSS_PARAM="--no.data.loss"
-fi
-
-echo "data loss param: ${DATA_LOSS_PARAM}"
+MIN_REFACTORED_MIRROR_MAKER_VERSION=2
+KAFKA_VERSION=$(grep "^version=" $KAFKA_HOME/cloudera/cdh_version.properties | cut -d '=' -f 2)
+KAFKA_MAJOR_VERSION=$(echo $KAFKA_VERSION | cut -d '-' -f 2 | sed 's/kafka//g' | cut -d '.' -f 1)
+echo "Kafka version found: ${KAFKA_VERSION}"
 
 if [[ -n ${WHITELIST} ]]; then
     WL_TOPICS="--whitelist ${WHITELIST}"
     echo "Using topic whitelist: ${WL_TOPICS}"
 fi
 
-if [[ -n ${BLACKLIST} ]]; then
-    BL_TOPICS="--blacklist ${BLACKLIST}"
-    echo "Using topic blacklist ${BL_TOPICS}"
-fi
-
-if [[ -n ${NUM_PRODUCERS} ]]; then
-    PRODUCER_PARAM="--num.producers ${NUM_PRODUCERS}"
-fi
-
 if [[ -n ${NUM_STREAMS} ]]; then
     STREAM_PARAM="--num.streams ${NUM_STREAMS}"
 fi
 
-if [[ -n ${QUEUE_SIZE} ]]; then
-    QUEUE_SIZE_PARAM="--queue.size ${QUEUE_SIZE}"
-fi
+if [[ $KAFKA_MAJOR_VERSION < $MIN_REFACTORED_MIRROR_MAKER_VERSION ]]; then
+    # Generating Zookeeper quorum
+    QUORUM=$ZK_QUORUM
+    if [[ -n $CHROOT ]]; then
+        QUORUM="${QUORUM}${CHROOT}"
+    fi
+    echo "Final Zookeeper Quorum is $QUORUM"
 
-if [[ -n ${QUEUE_BYTE_SIZE} ]]; then
-    QUEUE_BYTE_SIZE_PARAM="--queue.byte.size ${QUEUE_BYTE_SIZE}"
+    if ! grep zookeeper.connect= ${CONF_DIR}/mirror_maker_consumers.properties; then
+        echo "zookeeper.connect=$QUORUM" >> ${CONF_DIR}/mirror_maker_consumers.properties
+    fi
+
+    if [[ ${NO_DATA_LOSS} == "true" ]]; then
+        DATA_LOSS_PARAM="--no.data.loss"
+    fi
+
+    echo "data loss param: ${DATA_LOSS_PARAM}"
+
+    if [[ -n ${BLACKLIST} ]]; then
+        BL_TOPICS="--blacklist ${BLACKLIST}"
+        echo "Using topic blacklist ${BL_TOPICS}"
+    fi
+
+    if [[ -n ${NUM_PRODUCERS} ]]; then
+        PRODUCER_PARAM="--num.producers ${NUM_PRODUCERS}"
+    fi
+
+    if [[ -n ${QUEUE_SIZE} ]]; then
+        QUEUE_SIZE_PARAM="--queue.size ${QUEUE_SIZE}"
+    fi
+
+    if [[ -n ${QUEUE_BYTE_SIZE} ]]; then
+        QUEUE_BYTE_SIZE_PARAM="--queue.byte.size ${QUEUE_BYTE_SIZE}"
+    fi
+else
+    if [[ -n ${ABORT_ON_SEND_FAILURE} ]]; then
+        ABORT_ON_SEND_FAILURE_FLAG="--abort.on.send.failure ${ABORT_ON_SEND_FAILURE}"
+    fi
+
+    if [[ -n ${OFFSET_COMMIT_INTERVAL_MS} ]]; then
+        OFFSET_COMMIT_INTERVAL_MS_PARAM="--offset.commit.interval.ms ${OFFSET_COMMIT_INTERVAL_MS}"
+    fi
+
+    if [[ -n ${CONSUMER_REBALANCE_LISTENER} ]]; then
+        CONSUMER_REBALANCE_LISTENER_PARAM="--consumer.rebalance.listener ${CONSUMER_REBALANCE_LISTENER}"
+        if [[ -n ${CONSUMER_REBALANCE_LISTENER_ARGS} ]]; then
+            CONSUMER_REBALANCE_LISTENER_ARGS_PARAM="--consumer.rebalance.listener.args ${CONSUMER_REBALANCE_LISTENER_ARGS}"
+        fi
+    fi
+
+    if [[ -n ${MESSAGE_HANDLER} ]]; then
+        MESSAGE_HANDLER_PARAM="--message.handler ${MESSAGE_HANDLER}"
+        if [[ -n ${MESSAGE_HANDLER_ARGS} ]]; then
+            MESSAGE_HANDLER_ARGS_PARAM="--message.handler.args ${MESSAGE_HANDLER_ARGS}"
+        fi
+    fi
+
+    if [[ ${SOURCE_SECURITY_PROTOCOL} == *"SSL"* ]]; then
+        set +x
+        # Append other ssl params from ssl.properties
+        SSL_CONFIGS=$(cat ssl_client.properties)
+        if [[ ${SOURCE_SSL_CLIENT_AUTH} == "true" ]]; then
+            SSL_SERVER_CONFIGS=$(cat ssl_server.properties)
+            SSL_CONFIGS="${SSL_CONFIGS}
+${SSL_SERVER_CONFIGS}"
+        fi
+
+        # Replace SSL_CONFIGS's placeholder
+        perl -pi -e "s#\#ssl.configs={{SSL_CONFIGS}}#${SSL_CONFIGS}#" $CONF_DIR/mirror_maker_consumers.properties
+        set -x
+    fi
+
+    if [[ ${DESTINATION_SECURITY_PROTOCOL} == *"SSL"* ]]; then
+        set +x
+        # Append other ssl params from ssl.properties
+        SSL_CONFIGS=$(cat ssl_client.properties)
+        if [[ ${DESTINATION_SSL_CLIENT_AUTH} == "true" ]]; then
+            SSL_SERVER_CONFIGS=$(cat ssl_server.properties)
+            SSL_CONFIGS="${SSL_CONFIGS}
+${SSL_SERVER_CONFIGS}"
+        fi
+
+        # If user has not provided safety valve, replace SSL_CONFIGS's placeholder
+        perl -pi -e "s#\#ssl.configs={{SSL_CONFIGS}}#${SSL_CONFIGS}#" $CONF_DIR/mirror_maker_producers.properties
+        set -x
+    fi
+
+    if [[ ${SOURCE_SECURITY_PROTOCOL} == *"SASL"* || ${DESTINATION_SECURITY_PROTOCOL} == *"SASL"* ]]; then
+        if [[ -z "${JAAS_CONFIGS}" ]]; then
+            KEYTAB_FILE="${CONF_DIR}/kafka.keytab"
+            JAAS_CONFIGS="KafkaClient {
+    com.sun.security.auth.module.Krb5LoginModule required
+    useKeyTab=true
+    storeKey=true
+    keyTab=\"$KEYTAB_FILE\"
+    principal=\"$KAFKA_MIRROR_MAKER_PRINCIPAL\";
+};"
+        fi
+        echo "${JAAS_CONFIGS}" > $CONF_DIR/jaas.conf
+
+        export KAFKA_OPTS="${KAFKA_OPTS} -Djava.security.auth.login.config=${CONF_DIR}/jaas.conf"
+    fi
 fi
 
 # Propoagating logger information to Kafka
@@ -87,4 +173,8 @@ export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:$CONF_DIR/log4j.properties"
 export LOG_DIR=`pwd`
 
 # And finally run Kafka MirrorMaker itself
-exec $KAFKA_HOME/bin/kafka-mirror-maker.sh --new.producer ${WL_TOPICS} ${BL_TOPICS} ${DATA_LOSS_PARAM} ${PRODUCER_PARAM} ${STREAM_PARAM} ${QUEUE_SIZE_PARAM} ${QUEUE_BYTE_SIZE_PARAM} --consumer.config $CONF_DIR/mirror_maker_consumers.properties --producer.config $CONF_DIR/mirror_maker_producers.properties
+if [[ $KAFKA_MAJOR_VERSION < $MIN_REFACTORED_MIRROR_MAKER_VERSION ]]; then
+    exec $KAFKA_HOME/bin/kafka-mirror-maker.sh --new.producer ${WL_TOPICS} ${BL_TOPICS} ${DATA_LOSS_PARAM} ${PRODUCER_PARAM} ${STREAM_PARAM} ${QUEUE_SIZE_PARAM} ${QUEUE_BYTE_SIZE_PARAM} --consumer.config $CONF_DIR/mirror_maker_consumers.properties --producer.config $CONF_DIR/mirror_maker_producers.properties
+else
+    exec $KAFKA_HOME/bin/kafka-mirror-maker.sh ${ABORT_ON_SEND_FAILURE_FLAG} ${WL_TOPICS} --new.consumer ${STREAM_PARAM} ${OFFSET_COMMIT_INTERVAL_MS_PARAM} ${CONSUMER_REBALANCE_LISTENER_PARAM} ${CONSUMER_REBALANCE_LISTENER_ARGS_PARAM} ${MESSAGE_HANDLER_PARAM} ${MESSAGE_HANDLER_ARGS_PARAM} --consumer.config $CONF_DIR/mirror_maker_consumers.properties --producer.config $CONF_DIR/mirror_maker_producers.properties
+fi
